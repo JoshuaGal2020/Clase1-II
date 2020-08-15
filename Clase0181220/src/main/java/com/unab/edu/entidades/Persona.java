bf2b275bac06187fcadcc4df3ce28d8e7730f42a
JoshuaGal2020 <gg0210032019@unab.edu.sv>
2020-08-15 17:06:42 -0600

    Clase1 Ciclo II

---
+++ Clase0181220/src/main/java/com/unab/edu/entidades/Persona.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.edu.entidades;
+
+import lombok.Data;
+/**
+ *
+ * @author patty
+ */
+@Data
+public class Persona {
+    
+    
+    private int Idpersona;
+    private String Nombre;
+    private String Apellido;
+    private int Edad;
+    private String Sexo;
+}
